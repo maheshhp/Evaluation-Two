ca617d89de728301cb52089db59bf7aabf60aa7e
maheshhp <hpmahesh73@gmail.com>
2018-02-05 11:23:13 +0530

    models - Initialized sequelize; apis - Getting response from API 1

--- routes/apis.js
+++ routes/apis.js
@@ -12,7 +12,8 @@ module.exports = [{
   path: '/books/booksWithRatings',
   handler: (request, response) => {
     let apiOneResponse = '';
-    let apiTwoResponse = '';
+    const apiTwoResponse = '';
+    const bookRatings = {};
     https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', (extApiResponse) => {
       extApiResponse.setEncoding('UTF8');
       extApiResponse.on('data', (data) => {
@@ -20,17 +21,6 @@ module.exports = [{
       });
       extApiResponse.on('end', (end) => {
         apiOneResponse = JSON.parse(apiOneResponse);
-        console.log(apiOneResponse);
-      });
-    });
-    https.get('', (extApiResponse) => {
-      extApiResponse.setEncoding('UTF8');
-      extApiResponse.on('data', (data) => {
-        apiTwoResponse += data.toString();
-      });
-      extApiResponse.on('end', (end) => {
-        apiTwoResponse = JSON.parse(apiOneResponse);
-        console.log(apiTwoResponse);
       });
     });
     response(false);
