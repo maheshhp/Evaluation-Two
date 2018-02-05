f7f54e4405ea2fe0583051b743c2e7e422465c49
maheshhp <hpmahesh73@gmail.com>
2018-02-05 12:27:12 +0530

    test - Verified the booksWithRatings API response

--- routes/apis.js
+++ routes/apis.js
@@ -12,8 +12,7 @@ module.exports = [{
   path: '/books/booksWithRatings',
   handler: (request, response) => {
     let apiOneResponse = '';
-    const apiTwoResponse = '';
-    const bookRatings = {};
+    const newBookJson = {};
     https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', (extApiResponse) => {
       extApiResponse.setEncoding('UTF8');
       extApiResponse.on('data', (data) => {
@@ -21,8 +20,47 @@ module.exports = [{
       });
       extApiResponse.on('end', (end) => {
         apiOneResponse = JSON.parse(apiOneResponse);
+        let bookIds = Object.keys(apiOneResponse.books);
+        const promiseArray = [];
+        bookIds = bookIds.map(bid => apiOneResponse.books[bid].id);
+        apiOneResponse.books.forEach((item) => {
+          promiseArray.push(new Promise((resolve, reject) => {
+            https.get(`https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${item.id}`, (secResponse) => {
+              secResponse.setEncoding('UTF8');
+              const newBookObject = {
+                id: 0,
+                rating: 0,
+                author: '',
+                name: '',
+              };
+              secResponse.on('data', (data) => {
+                newBookObject.rating = JSON.parse(data.toString()).rating;
+              });
+              secResponse.on('end', (end) => {
+                newBookObject.id = item.id;
+                newBookObject.name = item.Name;
+                newBookObject.author = item.Author;
+                resolve(newBookObject);
+              });
+            });
+          }));
+        });
+        Promise.all(promiseArray).then((values) => {
+          const finalBookJson = {};
+          values.forEach((item) => {
+            if (typeof finalBookJson[item.author] === 'undefined') {
+              finalBookJson[item.author] = [];
+            }
+            finalBookJson[item.author].push({
+              author: item.author,
+              id: item.id,
+              name: item.name,
+              rating: item.rating,
+            });
+          });
+          response(finalBookJson);
+        });
       });
     });
-    response(false);
   },
 }];
