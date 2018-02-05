092cdff0dd5c2a75ca0725fd1524ffb7f3b5c072
maheshhp <hpmahesh73@gmail.com>
2018-02-05 10:26:03 +0530

    test - Added test for checking data response from booksWithRatings route

--- server.test.js
+++ server.test.js
@@ -2,7 +2,7 @@ const Server = require('./server');
 const Routes = require('./routes');
 
 describe('Testing the Hapi server that processes the requests for API calls', () => {
-  test('Should contain correct number of routes', () => {
+  test('Should contain correct number of routes at server and routes file', () => {
     expect(Routes.length).toBe(Server.table('localhost')[0].table.length);
   });
   test('Should return 200 status code for sucessful GET request', (done) => {
@@ -12,6 +12,16 @@ describe('Testing the Hapi server that processes the requests for API calls', ()
     };
     Server.inject(request, (response) => {
       expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+  test('Should return all books from Amazon API link with ratings on a sucessful GET request', (done) => {
+    const request = {
+      method: 'GET',
+      url: '/books/booksWithRatings',
+    };
+    Server.inject(request, (response) => {
+      expect(response.result).toBe(false);
       done();
     });
   });
